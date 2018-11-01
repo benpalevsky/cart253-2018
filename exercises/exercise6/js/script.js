b551837bc0066ea11a667864843d3082e346e484
Benjamin Palevsky <bpalevsky@gmail.com>
2018-11-01 13:53:28 -0400

    EX6: Added missing bracket

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -18,39 +18,41 @@ var rightPaddle;
 //
 // Creates the ball and paddles
 function setup() {
-  crateCanvas(640,480);
-  noStroke();
-  // Create a ball
-  ball = new Ball(width/2,height/2,50,50,10,50);
-  // Create the right paddle with UP and DOWN as controls
-  rightPaddle = new Paddle(width-10,height/2,10,600,10,UP_ARROW,DOWN_ARROW);
-  // Create the left paddle with W and S as controls
-  // Keycodes 83 and 87 are W and S respectively
-  leftPaddle = new Paddle(0,height/2,10,60,10,83,87;
+    crateCanvas(640, 480);
+    noStroke();
+    // Create a ball
+    ball = new Ball(width / 2, height / 2, 50, 50, 10, 50);
+    // Create the right paddle with UP and DOWN as controls
+    rightPaddle = new Paddle(width - 10, height / 2, 10, 600, 10, UP_ARROW, DOWN_ARROW);
+    // Create the left paddle with W and S as controls
+    // Keycodes 83 and 87 are W and S respectively
+
+    //////////////// FIXED
+    leftPaddle = new Paddle(0, height / 2, 10, 60, 10, 83, 87);
 
 
-// draw()
-//
-// Handles input, updates all the elements, checks for collisions
-// and displays everything.
-function draw() {
-  background(0);
+    // draw()
+    //
+    // Handles input, updates all the elements, checks for collisions
+    // and displays everything.
+    function draw() {
+        background(0);
 
-  leftPaddle.handleInput();
-  rightPaddle.handleInput();
+        leftPaddle.handleInput();
+        rightPaddle.handleInput();
 
-  ball.update;
-  leftPaddle.update();
-  rightPaddle.update();
+        ball.update;
+        leftPaddle.update();
+        rightPaddle.update();
 
-  if (ball.isOffTheScreen())
-    reset();
-  }
+        if (ball.isOffTheScreen())
+            reset();
+    }
 
-  ball.handleCollision(leftPaddle);
-  ball.handleCollision(rightPaddle);
+    ball.handleCollision(leftPaddle);
+    ball.handleCollision(rightPaddle);
 
-  ball.display();
-  leftPaddle.display();
-  rightPaddle.display(;
-}
+    ball.display();
+    leftPaddle.display();
+    rightPaddle.display(;
+    }
