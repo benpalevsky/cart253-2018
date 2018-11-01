654b28d5bc6b2219098b0ef87a487bcc266b30ed
Benjamin Palevsky <bpalevsky@gmail.com>
2018-11-01 13:47:42 -0400

    EX6: Fixed protyp typo and extra bracket in function declaration

--- exercises/exercise6/js/Ball.js
+++ exercises/exercise6/js/Ball.js
@@ -63,24 +63,26 @@ Ball.prototype.display = function() {
 //
 // Check if this ball overlaps the paddle passed as an argument
 // and if so reverse x velocity to bounce
-Ball.prototyp.handleCollision = function((paddle) {
-            // Check if the ball overlaps the paddle on x axis
-            if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
-                // Check if the ball overlaps the paddle on y axis
-                if (this.y + this.size > paddle.y && this.y < paddle.y + paddle.h) {
-                    // If so, move ball back to previous position (by subtracting current velocity)
-                    this.x -= this.vx;
-                    this.y -= this.vy;
-                    // Reverse x velocity to bounce
-                    this.vx = this.vx;
-                }
-            }
-        }
 
-        // reset()
-        //
-        // Set position back to the middle of the screen
-        Ball.prototype.rest = function() {
-            this.x = width / 2;
-            this.y = height / 2;
+//////////////// FIXED
+Ball.prototype.handleCollision = function(paddle) {
+    // Check if the ball overlaps the paddle on x axis
+    if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
+        // Check if the ball overlaps the paddle on y axis
+        if (this.y + this.size > paddle.y && this.y < paddle.y + paddle.h) {
+            // If so, move ball back to previous position (by subtracting current velocity)
+            this.x -= this.vx;
+            this.y -= this.vy;
+            // Reverse x velocity to bounce
+            this.vx = this.vx;
         }
+    }
+}
+
+// reset()
+//
+// Set position back to the middle of the screen
+Ball.prototype.rest = function() {
+    this.x = width / 2;
+    this.y = height / 2;
+}
