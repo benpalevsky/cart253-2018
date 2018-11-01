1e56bd4fa64d16b35c0b5c2c93f1e8e597b54327
Benjamin Palevsky <bpalevsky@gmail.com>
2018-11-01 00:41:03 -0400

    EX6: Fixed a hanful of bugs (sorry realized I should have been commiting each one)

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -3,45 +3,45 @@
 // A class that defines how a paddle behaves, including the ability
 // to specify the input keys to move it up and down
 
-Paddle constructor
+// Paddle constructor
 
-Sets the properties with the provided arguments or defaults
-function Pladdle(x,y,w,h,speed,downKey,upKey) {
-  this.x = x;
-  this.y = y;
-  this.xv = 0;
-  this.yv = 0;
-  this.w = w;
-  this.h = h;
-  this.speed = speeed;
-  this.downKey = downKey;
-  this.upKey = upKey;
+// Sets the properties with the provided arguments or defaults
+
+function Paddle(x, y, w, h, speed, downKey, upKey) {
+    this.x = x;
+    this.y = y;
+    this.xv = 0;
+    this.yv = 0;
+    this.w = w;
+    this.h = h;
+    this.speed = speed;
+    this.downKey = downKey;
+    this.upKey = upKey;
 }
 
 // handleInput()
 //
 // Check if the up or down keys are pressed and update velocity
 // appropriately
-Paddle.proto.handleInput = function() {
-  if (keyDown(upKey)) {
-    this.vy = -this.speed;
-  }
-  else if (keyDown(downKey)) {
-    this.vy = -this.speed;
-  }
+Paddle.prototype.handleInput = function() {
+    if (keyIsDown(this.upKey)) {
+        this.vy = -this.speed;
+    } else if (keyIsDown(this.downKey)) {
+        this.vy = -this.speed;
+    }
 }
 
 // update()
 // Update y position based on velocity
 // Constrain the resulting position to be within the canvas
 Paddle.prototype.update = function() {
-  this.y += this.vy;
-  this.y = constraint(this.y,0,hight-this.h);
+    this.y += this.vy;
+    this.y = constrain(this.y, 0, height - this.h);
 }
 
 // display()
 //
 // Draw the paddle as a rectangle on the screen
-Paddle.prototype.disploy = function()) {
-  rectangle(this.x,this.y,this.w,this.h);
+Paddle.prototype.disploy = function() {
+    rectangle(this.x, this.y, this.w, this.h);
 }
