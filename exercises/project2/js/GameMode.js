55653c0388e6ed8e0ce0018257a0d835d3337991
Benjamin Palevsky <bpalevsky@gmail.com>
2018-11-04 17:56:06 -0500

    P2: Finished the breakout mode and moved the update functions into the GameMode class

--- exercises/project2/js/GameMode.js
+++ exercises/project2/js/GameMode.js
@@ -2,9 +2,57 @@ function GameMode(type) {
     this.type = type;
 }
 
-GameMode.prototype.setup = function(left, right) {
+GameMode.prototype.setup = function(left, right, bumpers) {
     if (this.type == "REGULAR") {
         leftPaddle = new Paddle(0, height / 2, 10, 60, 10, 83, 87, "LEFT");
         rightPaddle = new Paddle(width - 10, height / 2, 10, 60, 10, DOWN_ARROW, UP_ARROW, "RIGHT");
+        ball = new Ball(width / 2, height / 2, 5, 5, 10, 5);
+    } else if (this.type == "BREAKOUT") {
+        leftPaddle = new Paddle(0, height / 4, 10, 60, 10, 83, 87, "LEFT");
+        rightPaddle = new Paddle(0, height - height / 4, 10, 60, 10, DOWN_ARROW, UP_ARROW, "RIGHT");
+        ball = new Ball(width / 2, height / 2, -5, -5, 10, 5);
+
+
+        var i = 0;
+        var j = 0;
+
+        bumperSize = 40;
+        numberOfBumpersX = (width * 3 / 4) / bumperSize;
+        numberOfBumpersY = height / bumperSize;
+
+
+        for (i = 0; i < numberOfBumpersY; i++) {
+            bumpers[i] = [];
+            for (j = 0; j < numberOfBumpersX; j++) {
+                bumpers[i][j] = new Paddle(width / 4 + (j * bumperSize), (i * bumperSize), bumperSize, bumperSize, 0, 0, 0, "BUMPER");
+            }
+        }
+    }
+}
+
+
+GameMode.prototype.update = function() {
+
+    ball.update();
+    leftPaddle.update();
+    rightPaddle.update();
+
+    if (ball.isOffScreen() > 0) {
+        scoreboard.update(leftPaddle);
+        ball.reset();
+    } else if (ball.isOffScreen() < 0) {
+        scoreboard.update(rightPaddle);
+        ball.reset();
+    }
+
+    if (this.type == "BREAKOUT") {
+
+        for (i = 0; i < numberOfBumpersY; i++) {;
+            for (j = 0; j < numberOfBumpersX; j++) {
+                bumpers[i][j].display();
+                ball.handleCollision(bumpers[i][j]);
+            }
+        }
+
     }
 }
--- exercises/project2/js/script.js
+++ exercises/project2/js/script.js
@@ -1,22 +1,14 @@
-// Basic OO Pong
-// by Pippin Barr
-//
-// A primitive implementation of Pong with no scoring system
-// just the ability to play the game with the keyboard.
-//
-// Arrow keys control the right hand paddle, W and S control
-// the left hand paddle.
-//
 // Written with JavaScript OOP.
 
 // Variable to contain the objects representing our ball and paddles
-var numberOfBumpers = 50;
 var bumpers = [];
+
 var scoreboard;
 var leftPaddle;
 var rightPaddle;
+var ball;
 var gameIsActive = false;
-var gameMode;
+var currentGame;
 
 // setup()
 //
@@ -27,38 +19,22 @@ function setup() {
 
     fill(255);
 
-    var i = 0;
-    numberOfBumpers = height / 20;
 
-    for (i = 0; i < numberOfBumpers; i++) {
-        bumpers[i] = new Paddle(width / 2, (i * numberOfBumpers), 20, 20, 0, 0, 0, "BUMPER");
-    }
 
 
 
     // Create a scoreboard
     scoreboard = new Scoreboard(0, 0);
 
-    // Create a ball
-    ball = new Ball(width / 2, height / 2, 5, 5, 10, 5);
 
     // Create the game
 
-    gameMode = new GameMode("REGULAR");
-    gameMode.setup(leftPaddle, rightPaddle);
-
-    // Create the left paddle with W and S as controls
-    // Keycodes 83 and 87 are W and S respectively
-    //leftPaddle = new Paddle(0, height / 2, 10, 60, 10, 83, 87, "LEFT");
-    //
-    //rightPaddle = new Paddle(width - 10, height / 2, 10, 60, 10, DOWN_ARROW, UP_ARROW, "RIGHT");
+    currentGame = new GameMode("BREAKOUT");
+    currentGame.setup(leftPaddle, rightPaddle, bumpers);
 
 }
 
-// draw()
-//
-// Handles input, updates all the elements, checks for collisions
-// and displays everything.
+
 function draw() {
 
 
@@ -70,28 +46,17 @@ function draw() {
 
     if (gameIsActive) {
 
+
         leftPaddle.handleInput();
         rightPaddle.handleInput();
 
-        ball.update();
-        leftPaddle.update();
-        rightPaddle.update();
+        currentGame.update();
 
-        if (ball.isOffScreen() > 0) {
-            scoreboard.update(leftPaddle);
-            ball.reset();
-        } else if (ball.isOffScreen() < 0) {
-            scoreboard.update(rightPaddle);
-            ball.reset();
-        }
+
 
         ball.handleCollision(leftPaddle);
         ball.handleCollision(rightPaddle);
 
-        for (i = 0; i < numberOfBumpers; i++) {
-            bumpers[i].display();
-            ball.handleCollision(bumpers[i]);
-        }
 
         display();
 
