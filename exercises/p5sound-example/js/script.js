2bebdaf8b7a87cfc2c8beeebf2c51ee48ccd0e6a
Benjamin Palevsky <bpalevsky@gmail.com>
2018-11-15 15:35:29 -0500

    P3: Project Import

---
+++ exercises/p5sound-example/js/script.js
@@ -0,0 +1,31 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// setup()
+//
+// Description of setup
+
+function preload() {
+
+
+}
+
+function setup() {
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
