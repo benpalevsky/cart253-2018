46e9a4478d5d0d25b22cb4e74c7a2d2fb270d671
Benjamin Palevsky <bpalevsky@gmail.com>
2018-11-24 22:08:22 -0500

    P3: Have a stable-r version of the prototype I want to move forward with

--- exercises/project3/js/script.js
+++ exercises/project3/js/script.js
@@ -25,18 +25,20 @@ done. Ultimately what I'm after is something polished that's fun on both mobile 
 //
 // Description of setup
 
-var midiNotes = [52, 54, 64, 66, 52, 54, 64, 66, 52, 54, 64, 66, 52, 54, 64, 66, 52, 54, 64, 66];
+var notes = ["c4", "d4", "e4", "g4"];
 var drums;
 var a;
 
 var slider = []
-var numberOfSliders = 16;
+var numberOfSliders = 4;
 
-
+var pauseMusic = true;
 
 var cutoff;
 var counter = 0;
-var rate = 1.5;
+var rate = 4;
+
+var phrase;
 
 // @Ben need to convert everything to beats or quarter notes or whatever
 // also better variable names rate --> bpm or tempo
@@ -45,24 +47,65 @@ var rate = 1.5;
 function setup() {
 
 
-    drums = Gibber.Percussion.EDrums('x*ox*xo-');
-    Gibber.Clock.rate = 1;
-    a = Sine();
-    a.frequency.seq([220, 440], [1 / 4, 1 / 8, 1 / 4]);
+    createCanvas(512, 512);
+    background(100);
+
+    Gibber.timeSignature = "4/4";
+    Gibber.Clock.bpm = 120;
+    Gibber.Clock.rate = 4;
 
 
-    createCanvas(1024, 512);
-    background(255);
+    phrase = "xxx-xxxo";
 
+    // for (i = 0; i < phrase.length; i++) {
+    //     fill(255);
+    //     noStroke();
+    //     rect(width / phrase.length * i, 0, 10, height);
+    // }
+
+    // will not wrap the drums
+    //drums = Gibber.Percussion.EDrums(phrase, 4 / 4);
+
+    //will wrap the drums
+    //drums = Gibber.Percussion.EDrums(phrase);
+
+    //a = Sine();
+    //a.frequency.seq([220, 440], [1 / 4, 1 / 8, 1 / 4]);
+
+    b = FM({
+        attack: ms(1)
+    });
+
+    //b.note.seq(['a2', 'bb2', 'c2', 'd2'].random(), [1 / 8, 1 / 4].random(1 / 8, 2));
+
+
+
+    //createSlider(1, 4, 1, 1);
     fill(255, 0, 0);
     cutoff = width - 128;
-    rect(cutoff, 0, 10, height);
+
+    fill(0);
+
+    d = Synth();
+    d.attack = ms(1);
+
+
+
+
+
+
+
+
 
     for (i = 0; i < numberOfSliders; i++) {
         //slider[i] = createSlider(sliderMin, sliderMax, map(i, 0, numberOfSliders, 0, 255));
-        slider[i] = new Slider(0, width, 0 + (i * (width / numberOfSliders)), 0.1, midiNotes[i]);
-        slider[i].object.position(0, ((height / numberOfSliders) * i) + (height / 3 / numberOfSliders));
-        slider[i].object.style('width', width - 2 + "px");
+        slider[i] = createSlider(0, 65, i * 16, 1);
+        slider[i].position(0, ((height / numberOfSliders) * i) + (height / 3 / numberOfSliders));
+        slider[i].style('width', width - 2 + "px");
+
+        //synthesizer
+        slider[i].instrument = Synth();
+        slider[i].instrument.attack = ms(1);
     }
 
 
@@ -73,22 +116,40 @@ function setup() {
 function draw() {
 
 
-    for (i = 0; i < numberOfSliders; i++) {
-        if (slider[i].object.value() == slider[i].minValue)
-            slider[i].object.value(slider[i].minValue + 1)
-        else if (slider[i].object.value() == slider[i].minValue)
-            slider[i].object.value(slider[i].minValue - 1)
-        else
-            slider[i].object.value(slider[i].object.value() + rate);
-    }
 
-    for (i = 0; i < numberOfSliders; i++) {
-        if (slider[i].object.value() >= cutoff) {
-            //slider[i].playNote();
-            slider[i].reset();
+
+    if ((frameCount % rate == 0 || frameCount == 1) && (pauseMusic)) {
+
+        for (i = 0; i < numberOfSliders; i++) {
+            if (slider[i].value() > 64) {
+                slider[i].value(0);
+                slider[i].instrument.note(notes[i]);
+
+            } else
+                slider[i].value(slider[i].value() + 1);
         }
-    }
 
+    }
+    //createSlider(1, 4, 1, 1);
+
+    //rect(1, 1, width / 2, height / 2);
+    // for (i = 0; i < numberOfSliders; i++) {
+    //     if (slider[i].object.value() == slider[i].minValue)
+    //         slider[i].object.value(slider[i].minValue + 1)
+    //     else if (slider[i].object.value() == slider[i].minValue)
+    //         slider[i].object.value(slider[i].minValue - 1)
+    //     else
+    //         slider[i].object.value(slider[i].object.value() + rate);
+    // }
+    //
+    // for (i = 0; i < numberOfSliders; i++) {
+    //     if (slider[i].object.value() >= cutoff) {
+    //         //slider[i].playNote();
+    //         slider[i].reset();
+    //     }
+    // }
+    //a.frequency.seq([mouseX, mouseY], [1 / 4, 1 / 4]);
+    //drums.pitch = mouseX;
 
 
 }
