c30b794c88f3fa0bc5694a7ee12a17f7addcafd1
Benjamin Palevsky <bpalevsky@gmail.com>
2018-11-29 15:56:06 -0500

    P3: added four sliders and pause functionality

--- exercises/project3/prototype2/js/script.js
+++ exercises/project3/prototype2/js/script.js
@@ -26,7 +26,7 @@ done. Ultimately what I'm after is something polished that's fun on both mobile 
 // Description of setup
 
 var dragging = false;
-var rectX = 0;
+
 var lastX = 0;
 
 var offsetX = 0;
@@ -37,8 +37,27 @@ var offsetX = 0;
 
 
 
-var steps = 5;
+var steps = 16;
 
+//synth for each slider
+var aSynth;
+var bSynth;
+var cSynth;
+var dSynth;
+
+//location for each handle
+var ax = 0;
+var bx = 0;
+var cx = 0;
+var dx = 0;
+
+var aPause = false;
+var bPause = false;
+var cPause = false;
+var dPause = false;
+
+//location for selector
+var sy = 0;
 
 function setup() {
 
@@ -54,6 +73,17 @@ function setup() {
 
     fill(60, 150, 70);
 
+    aSynth = Synth();
+    aSynth.attack = ms(1);
+
+    bSynth = Synth();
+    bSynth.attack = ms(1);
+
+    cSynth = Synth();
+    cSynth.attack = ms(1);
+
+    dSynth = Synth();
+    dSynth.attack = ms(1);
 
 }
 
@@ -62,35 +92,133 @@ function draw() {
     background(100);
     fill(120);
 
-    //draw the horizontal track
-    rect(0, height / 2, width, 2);
-
     //draw the steps
-    for (i = 0; i < steps - 1; i++) {
-        rect((width / (steps)) * (i + 1), 0, 2, height);
+    for (j = 0; j < 3; j++) {
+
+        //draw the horizontal track
+        rect(0, height * (j + 1) / 4, width, 2);
+
+
+        for (i = 0; i < steps - 1; i++) {
+            rect((width / (steps)) * (i + 1), 0, 2, height);
+        }
     }
 
-    fill(60, 150, 70);
-
-
-
-    // drag
+    // drag controls
     if (dragging) {
-        rectX = lastX + (mouseX - offsetX);
+        ax = lastX + (mouseX - offsetX);
     }
 
+    //move the handle
+    if ((frameCount % 4 == 0 || frameCount == 1) && (!dragging)) {
+
+        if (!aPause)
+            ax += (width / steps);
+        if (!bPause)
+            bx += (width / steps);
+        if (!cPause)
+            cx += (width / steps);
+        if (!dPause)
+            dx += (width / steps);
 
 
-    if ((frameCount % 60 == 0 || frameCount == 1) && (!dragging)) {
-        rectX += (width / steps);
+        //play every beat
+        //aSynth.note('c4');
 
-        if (rectX > 512) {
-            rectX = 0;
+        //worldwrap
+        //play note
+        if (ax > ((width / steps) * (steps - 1))) {
+            ax = 0;
+            aSynth.note('a3');
+        }
+        if (bx > ((width / steps) * (steps - 1))) {
+            bx = 0;
+            bSynth.note('c3');
+        }
+        if (cx > ((width / steps) * (steps - 1))) {
+            cx = 0;
+            cSynth.note('g3');
+
+        }
+        if (dx > ((width / steps) * (steps - 1))) {
+            dx = 0;
+            dSynth.note('c4');
+
         }
 
+
+
+
     }
 
-    rect(rectX, 0, 30, height);
+
+    //draw everything
+    fill(60, 150, 70);
+    rect(ax, 0 * (height / 4), width / steps, height / 4);
+
+
+    fill(150, 60, 70);
+
+    rect(bx, 1 * (height / 4), width / steps, height / 4);
+    fill(60, 70, 150);
+
+    rect(cx, 2 * (height / 4), width / steps, height / 4);
+    fill(100, 140, 70);
+
+    rect(dx, 3 * (height / 4), width / steps, height / 4);
+
+    fill(255, 255, 255, 100);
+    rect(0, sy * (height / 4), width, height / 4);
+
+
+
+
+}
+
+//arrow key controls
+
+function keyPressed() {
+    if (keyCode === DOWN_ARROW) {
+        sy = constrain(++sy, 0, 3);
+    } else if (keyCode === UP_ARROW) {
+        sy = constrain(--sy, 0, 3);
+    } else if (keyCode === RIGHT_ARROW) {
+        switch (sy) {
+            case 0:
+                ax += (width / steps);
+                break;
+            case 1:
+                bx += (width / steps);
+                break;
+            case 2:
+                cx += (width / steps);
+                break;
+            case 3:
+                dx += (width / steps);
+                break;
+        }
+    } else if (keyCode === LEFT_ARROW) {
+        switch (sy) {
+            case 0:
+                ax -= (width / steps);
+                break;
+            case 1:
+                bx -= (width / steps);
+                break;
+            case 2:
+                cx -= (width / steps);
+                break;
+            case 3:
+                dx -= (width / steps);
+                break;
+
+        }
+    } else if (keyCode === 32) {
+        aPause = !aPause;
+        bPause = !bPause;
+        cPause = !cPause;
+        dPause = !dPause;
+    }
 
 }
 
@@ -103,9 +231,11 @@ function mousePressed() {
 
 }
 
+
+
 function mouseReleased() {
     // Stop dragging
     dragging = false;
-    lastX = rectX;
+    lastX = ax;
 
 }
